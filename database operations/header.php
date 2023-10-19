7668b16a671a4e730172f243b6644f97720e88af
Sreeja194 <143074208+Sreeja194@users.noreply.github.com>
2023-10-19 16:14:58 +0530

    Add files via upload

---
+++ database operations/header.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Student List</title>
+  <link rel="stylesheet" href="css/style.css">
+  <style>
+    #footer {
+      position: fixed; 
+      padding: 10px;
+      bottom: 0;
+      width: 100%;
+      height: 40px;
+    }
+  </style>
+</head>
+<body>
+  <div id="wrapper">
+    <div id="header">
+      <h1>Students List using CRUD Operations</h1>
+    </div>
+    <div id="menu">
+      <ul>
+        <li>
+          <a href="index.php">Home</a>
+        </li>
+        <li>
+          <a href="add.php">Add</a>
+        </li>
+        <li>
+          <a href="update.php">Update</a>
+        </li>
+        <li>
+          <a href="delete.php">Delete</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</body>
+</html>
